2420b8f01ef6034767f13012b54c669b4458f93e
Christian Geer <geerc@aldenv186.allegheny.edu>
2017-10-23 14:10:53 -0400

    adding heading titles for code not yet added

--- 8 x 1k/8x1k script.R
+++ 8 x 1k/8x1k script.R
@@ -16,3 +16,11 @@ Sep.28.2016 <- mutate(Sep.28.2016, Moving.Time = Time)
 Oct.12.2016 <- Oct.12.2016[-c(2, 4, 6), ] %>% select(Split, Moving.Time)
 Sep.26.2017 <- Sep.26.2017[-c(2, 4, 6, 8, 10, 12, 14, 16, 17), ] %>% select(Split, Moving.Time)
 Sep.28.2016 <- Sep.28.2016[-c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20), ] %>% select(Split, Moving.Time)
+
+# Rename Moving.Time to Time
+
+#Renaming splits so as not go by 2s
+
+# Reindex the rows to correct numbering
+
+
