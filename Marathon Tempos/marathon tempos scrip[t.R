e3691f15139d79e5ba51b2939ee53e80cd4d563c
Christian Geer <geerc@allegheny.edu>
2017-10-29 16:46:28 -0400

    changed import data code to laptop version

--- Marathon Tempos/marathon tempos scrip[t.R
+++ Marathon Tempos/marathon tempos scrip[t.R
@@ -3,10 +3,9 @@ library(tidyverse)
 library(dplyr)
 
 # Import data sets for workouts
-Sep.22.2017 <- read.csv("~/Running Data/Marathon Tempos/Sep 22 2017.csv")
-Sep.23.2016 <- read.csv("~/Running Data/Marathon Tempos/Sep 23 2016.csv")
+Sep.22.2017 <- read.csv("~/GitHub/workouts/Marathon Tempos/Sep 22 2017.csv")
+Sep.23.2016 <- read.csv("~/GitHub/workouts/Marathon Tempos/Sep 23 2016.csv")
 
 # Delete rest splits and remove unnecessary columns
 Sep.22.2017 <- Sep.22.2017[-c(1, 3, 4), ] %>% select(Split, Avg.Pace)
 Sep.23.2016 <- Sep.23.2016[-c(1, 2, 8, 9, 10), ] %>% select(Split, Avg.Pace)
-
