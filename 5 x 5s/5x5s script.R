fff1aee3f7ac8bffe8248c5ab2ae2eb60076ef03
Christian Geer <geerc@allegheny.edu>
2017-10-29 16:35:34 -0400

    Some changes for later when another workout is added

--- 5 x 5s/5x5s script.R
+++ 5 x 5s/5x5s script.R
@@ -3,8 +3,6 @@ library(tidyverse)
 library(dplyr)
 
 # Load data sets
-Oct.4.2016 <- read.csv("~/Running Data/5 x 5s/Oct 4 2016.csv") # Alden Computers
-
 Oct.4.2016 <- read.csv("~/Github/workouts/5 x 5s/Oct 4 2016.csv") # My labtop
 
 # Delete rest splits and remove unnecessary columns
@@ -19,10 +17,23 @@ Oct.4.2016[5, 1] <- 5
 # Reindex the rows to correct numbering
 rownames(Oct.4.2016) <- 1:nrow(Oct.4.2016)
 
+# Join data sets together
+# all.workouts <- full_join(Oct.4.2016, , by = "Split")
+
+# Rename column headers to date of workout
+# names(all.workouts)[names(all.workouts) == "Time.x"] <- "Oct 4 2016"
+
+# Make new table, optimized for plotting
+all.workouts.plot <- all.workouts.plot <- all.workouts %>% gather(`Oct 4 2016`, ``, , key = "Date", value = "Distance") %>% arrange(Split)
+
+# Convert split column from character to doubles, and sort by split
+all.workouts.plot$Split <- as.double(all.workouts.plot$Split)
+all.workouts.plot <- arrange(all.workouts.plot, Split)
+
 # Adding date so that we can group the observations together for plotting
 Oct.4.2016$Date <- "Oct 4 2016"
 
 # Plot the data
-ggplot(data = Oct.4.2016, mapping = aes(x = Split, y = Distance, group = Date, color = Date)) + 
-    geom_path() + 
+ggplot(data = Oct.4.2016, mapping = aes(x = Split, y = Distance, group = Date, color = Date)) +
+    geom_path() +
     ylim(.75, 1)
