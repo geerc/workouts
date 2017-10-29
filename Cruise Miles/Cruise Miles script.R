abdafd7312b6c803a3581b632e8a5b3073af374e
Christian Geer <geerc@allegheny.edu>
2017-10-29 16:45:36 -0400

    Got code up to date with split fix

--- Cruise Miles/Cruise Miles script.R
+++ Cruise Miles/Cruise Miles script.R
@@ -62,6 +62,10 @@ names(all.workouts)[names(all.workouts) == "Time"] <- "Oct 25 2016"
 # Make new data table optimized for plotting
 all.workouts.plot <- all.workouts %>% gather(`Oct 25 2016`, `Aug 11 2017`, `Aug 29 2017`, key = "Date", value = "Time") %>% arrange(Split)
 
+# Convert split column from char to double, and sort
+all.workouts.plot$Split <- as.double(all.workouts.plot$Split)
+all.workouts.plot <- arrange(all.workouts.plot, Split)
+
 #Plot it
-ggplot(data = all.workouts.plot, mapping = aes(x = Split, y = Time, group = Date, color = Date)) + 
+ggplot(data = all.workouts.plot, mapping = aes(x = Split, y = Time, group = Date, color = Date)) +
   geom_path()
