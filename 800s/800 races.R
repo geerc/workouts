47acd5d28ceda0e73db134218c1965b12a5490fa
Christian Geer <geerc@allegheny.edu>
2017-10-30 10:41:37 -0400

    Races added, code finished

--- 800s/800 races.R
+++ 800s/800 races.R
@@ -2,6 +2,11 @@
 library(tidyverse)
 library(dplyr)
 
+# Import data sets
+spire.2016<- read.csv("~/GitHub/workouts/800s/spire 2016.csv")
+mount.union.2016 <- read.csv("~/GitHub/workouts/800s/Mount Union 2016 (1).csv")
+tiffin.2016 <- read.csv("~/GitHub/workouts/800s/tiffin 2016.csv")
+
 # Create shell of table
 races.800s <- tibble(
   Meet = c(Spire, MountUnion1, Tiffin, Kenyon, Gator), 
