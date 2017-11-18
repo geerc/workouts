40d3b5be613170f3bfad15a0fc1cd362abc06de1
Christian Geer <geerc@allegheny.edu>
2017-11-18 17:15:48 -0500

    moved files around

--- Races/5ks.R
+++ Races/5ks.R
@@ -3,75 +3,54 @@ library(tidyverse)
 library(dplyr)
 
 # Import data sets for workouts
-grove.city.2016 <- read.csv("~/workouts/races/grove.city 2016.csv")
-wooster.2016 <- read.csv("~/workouts/races/wooster.2016.csv")
+grove.city.2016 <- read.csv("~/workouts/races/grove city 2016.csv")
+wooster.2016 <- read.csv("~/workouts/races/wooster 2016.csv")
 chatham.2017 <- read.csv("~/workouts/races/chatham 2017.csv")
 wooster.2017 <- read.csv("~/workouts/races/wooster 2017.csv")
 
 # Copy Time data to Moving.Time so that unneeded splits can be deleted
-oberlin.2017 <- mutate(oberlin.2017, Moving.Time = Time)
-paul.short.2017 <- mutate(paul.short.2017, Moving.Time = Time)
-allegheny.classic.2017 <- mutate(allegheny.classic.2017, Moving.Time = Time)
-rowan.2016 <- mutate(rowan.2016, Moving.Time = Time)
-paul.short.2016 <- mutate(paul.short.2016, Moving.Time = Time)
-st.vincent.2016 <- mutate(st.vincent.2016, Moving.Time = Time)
-conferences.2017 <- mutate(conferences.2017, Moving.Time = Time)
+grove.city.2016 <- mutate(grove.city.2016, Moving.Time = Time)
+wooster.2016 <- mutate(wooster.2016, Moving.Time = Time)
+chatham.2017 <- mutate(chatham.2017, Moving.Time = Time)
+wooster.2017 <- mutate(wooster.2017, Moving.Time = Time)
 
 # Delete all splits, leaving just the final time
-oberlin.2017 <- oberlin.2017[-c(1), ] %>% select(Split, Moving.Time)
-paul.short.2017 <- paul.short.2017[-c(1, 2, 3, 4, 5), ] %>% select(Split, Moving.Time)
-allegheny.classic.2017 <- allegheny.classic.2017[-c(1, 2, 3, 4), ] %>% select(Split, Moving.Time)
-rowan.2016 <- rowan.2016[-c(1), ] %>% select(Split, Moving.Time)
-paul.short.2016 <- paul.short.2016[-c(1), ] %>% select(Split, Moving.Time)
-st.vincent.2016 <- st.vincent.2016[-c(1), ] %>% select(Split, Moving.Time)
-conferences.2017 <- conferences.2017[-c(1), ] %>% select(Split, Moving.Time)
+grove.city.2016 <- grove.city.2016[-c(1:4), ] %>% select(Split, Moving.Time)
+wooster.2016 <- wooster.2016[-c(1:4), ] %>% select(Split, Moving.Time)
+chatham.2017 <- chatham.2017[-c(1:4), ] %>% select(Split, Moving.Time)
+wooster.2017 <- wooster.2017[-c(1), ] %>% select(Split, Moving.Time)
 
 # Rename Moving.Time to Time
-names(oberlin.2017)[names(oberlin.2017) == "Moving.Time"] <- "Time"
-names(paul.short.2017)[names(paul.short.2017) == "Moving.Time"] <- "Time"
-names(allegheny.classic.2017)[names(allegheny.classic.2017) == "Moving.Time"] <- "Time"
-names(rowan.2016)[names(rowan.2016) == "Moving.Time"] <- "Time"
-names(paul.short.2016)[names(paul.short.2016) == "Moving.Time"] <- "Time"
-names(st.vincent.2016)[names(st.vincent.2016) == "Moving.Time"] <- "Time"
-names(conferences.2017)[names(conferences.2017) == "Moving.Time"] <- "Time"
+names(grove.city.2016)[names(grove.city.2016) == "Moving.Time"] <- "Time"
+names(wooster.2016)[names(wooster.2016) == "Moving.Time"] <- "Time"
+names(chatham.2017)[names(chatham.2017) == "Moving.Time"] <- "Time"
+names(wooster.2017)[names(wooster.2017) == "Moving.Time"] <- "Time"
 
 # Reindex rows to correct numbering
-rownames(oberlin.2017) <- 1:nrow(oberlin.2017)
-rownames(paul.short.2017) <- 1:nrow(paul.short.2017)
-rownames(allegheny.classic.2017) <- 1:nrow(allegheny.classic.2017)
-rownames(rowan.2016) <- 1:nrow(rowan.2016)
-rownames(paul.short.2016) <- 1:nrow(paul.short.2016)
-rownames(st.vincent.2016) <- 1:nrow(st.vincent.2016)
-rownames(conferences.2017) <- 1:nrow(conferences.2017)
+rownames(grove.city.2016) <- 1:nrow(grove.city.2016)
+rownames(wooster.2016) <- 1:nrow(wooster.2016)
+rownames(chatham.2017) <- 1:nrow(chatham.2017)
+rownames(wooster.2017) <- 1:nrow(wooster.2017)
 
 # Join data sets together
-all.workouts <- full_join(st.vincent.2016, paul.short.2016, by = "Split") %>%
-  full_join(rowan.2016, by = "Split") %>%
-  full_join(allegheny.classic.2017, by = "Split") %>%
-  full_join(paul.short.2017, by = "Split") %>%
-  full_join(oberlin.2017, by = "Split") %>%
-  full_join(conferences.2017, by = "Split")
+all.workouts <- full_join(grove.city.2016, wooster.2016, by = "Split") %>%
+  full_join(chatham.2017, by = "Split") %>%
+  full_join(wooster.2017, by = "Split")
 
 # Rename column headers to name of race
-names(all.workouts)[names(all.workouts) == "Time.x"] <- "St. Vincent 2016"
-names(all.workouts)[names(all.workouts) == "Time.y"] <- "Paul Short 2016"
-names(all.workouts)[names(all.workouts) == "Time.x.x"] <- "Rowan 2016"
-names(all.workouts)[names(all.workouts) == "Time.y.y"] <- "Allegheny Classic 2017"
-names(all.workouts)[names(all.workouts) == "Time.x.x.x"] <- "Paul Short 2017"
-names(all.workouts)[names(all.workouts) == "Time.y.y.y"]   <- "Oberlin 2017"
-names(all.workouts)[names(all.workouts) == "Time"] <- "Conferences 2017"
+names(all.workouts)[names(all.workouts) == "Time.x"] <- "Grove City 2016"
+names(all.workouts)[names(all.workouts) == "Time.y"] <- "Wooster 2016"
+names(all.workouts)[names(all.workouts) == "Time.x.x"] <- "Chatham 2017"
+names(all.workouts)[names(all.workouts) == "Time.y.y"] <- "Wooster 2017"
 
 # Mak new data table for plotting
-all.workouts.plot <- gather(all.workouts, `Conferences 2017`, `Oberlin 2017`, `Paul Short 2017`, `Allegheny Classic 2017`, `Rowan 2016`, `Paul Short 2016`, `St. Vincent 2016`, key = "Race", value = "Time")
+all.workouts.plot <- gather(all.workouts, `Wooster 2017`, `Chatham 2017`, `Wooster 2016`, `Grove City 2016`, key = "Race", value = "Time")
 
 # Make split column count up from 1
 all.workouts.plot[1, 1] <- 1
 all.workouts.plot[2, 1] <- 2
 all.workouts.plot[3, 1] <- 3
 all.workouts.plot[4, 1] <- 4
-all.workouts.plot[5, 1] <- 5
-all.workouts.plot[6, 1] <- 6
-all.workouts.plot[7, 1] <- 7
 
 # Conver split to double
 all.workouts.plot$Split <- as.double(all.workouts.plot$Split)
@@ -84,9 +63,6 @@ all.workouts.plot[1, 1] <- 1
 all.workouts.plot[2, 1] <- 2
 all.workouts.plot[3, 1] <- 3
 all.workouts.plot[4, 1] <- 4
-all.workouts.plot[5, 1] <- 5
-all.workouts.plot[6, 1] <- 6
-all.workouts.plot[7, 1] <- 7
 
 # New variable to allow grouping for plot
 all.workouts.plot[1, 4] <- 1
@@ -95,4 +71,4 @@ all.workouts.plot <- fill(all.workouts.plot, V4)
 # Plot
 ggplot(data = all.workouts.plot, mapping= aes(x = Split, y = Time, group = V4)) +
   geom_path() +
-  geom_vline(xintercept = 3.5)
+  geom_vline(xintercept = 2.5)
