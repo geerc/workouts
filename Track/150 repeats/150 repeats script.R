9811d7238f29634ce56991eba0b56ce857b052cb
Christian Geer <geerc@allegheny.edu>
2017-11-18 17:08:46 -0500

    fixed file path

--- Track/150 repeats/150 repeats script.R
+++ Track/150 repeats/150 repeats script.R
@@ -3,8 +3,8 @@ library(tidyverse)
 library(dplyr)
 
 # Import data sets for workouts
-feb.2.2017 <- read.csv("~/GitHub/workouts/Track/150 repeats/feb 2 2017.csv")
-apr.25.2017 <- read.csv("~/GitHub/workouts/Track/150 repeats/apr 25 2017.csv")
+feb.2.2017 <- read.csv("~/workouts/Track/150 repeats/feb 2 2017.csv")
+apr.25.2017 <- read.csv("~/workouts/Track/150 repeats/apr 25 2017.csv")
 
 # Copy Time data to Moving.Time, so we can delete rest splits
 feb.2.2017 <- mutate(feb.2.2017, Moving.Time = Time)
