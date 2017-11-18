1f315036fa54b67973c87b29471e97e830c63073
Christian Geer <geerc@allegheny.edu>
2017-11-18 17:03:07 -0500

    fixed file path

--- Broken Tempos/Broken Tempo script.R
+++ Broken Tempos/Broken Tempo script.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(dplyr)
 
 # Import data sets for workouts
-Sep.5.2017 <- read.csv("~/GitHub/workouts/Broken Tempos/Sep 5 2017.csv")
+Sep.5.2017 <- read.csv("~/workouts/Broken Tempos/Sep 5 2017.csv")
 
 # Delete rest splits and remove unnecessary columns
 Sep.5.2017 <- Sep.5.2017[-c(1, 2, 3, 6, 8, 10, 12, 14, 17, 19, 21, 23, 25, 26, 27, 28), ] %>% select(Split, Avg.Pace)
