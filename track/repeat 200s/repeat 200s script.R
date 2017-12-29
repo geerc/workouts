28ae0de58f57f63944760f60b81122746b4acdb4
Christian Geer <geerc@allegheny.edu>
2017-12-28 22:51:46 -0500

    added dec 27 workout

--- track/repeat 200s/repeat 200s script.R
+++ track/repeat 200s/repeat 200s script.R
@@ -12,6 +12,7 @@ mar.21.2017 <- read.csv("~/workouts/track/repeat 200s/mar 21 2017.csv")
 dec.7.2017 <- read.csv("~/workouts/track/repeat 200s/dec 7 2017.csv")
 dec.16.2017 <- read.csv("~/workouts/track/repeat 200s/dec 16 2017.csv")
 dec.21.2017 <- read.csv("~/workouts/track/repeat 200s/dec 21 2017.csv")
+dec.27.2017 <- read.csv("~/workouts/track/repeat 200s/dec 27 2017.csv")
 
 # Copy Time data to Moving.Time, so we can delete rest splits
 dec.9.2016 <- mutate(dec.9.2016, Moving.Time = Time)
@@ -22,6 +23,7 @@ mar.21.2017 <- mutate(mar.21.2017, Moving.Time = Time)
 dec.7.2017 <- mutate(dec.7.2017, Moving.Time = Time)
 dec.16.2017 <- mutate(dec.16.2017, Moving.Time = Time)
 dec.21.2017 <- mutate(dec.21.2017, Moving.Time = Time)
+dec.27.2017 <- mutate(dec.27.2017, Moving.Time = Time)
 
 # Delete rest splits and remove unnecessary columns
 dec.9.2016 <- dec.9.2016[-c(1:18), ] %>% select(Split, Moving.Time)
@@ -32,6 +34,7 @@ mar.3.2017 <- mar.3.2017[-c(1:26), ] %>% select(Split, Moving.Time)
 dec.7.2017 <- dec.7.2017[-c(1:17), ] %>% select(Split, Moving.Time)
 dec.16.2017 <- dec.16.2017[-c(1:17), ] %>% select(Split, Moving.Time)
 dec.21.2017 <- dec.21.2017[-c(1:17), ] %>% select(Split, Moving.Time)
+dec.27.2017 <- dec.27.2017[-c(1:17), ] %>% select(Split, Moving.Time)
 
 # Rename Moving.Time to Time
 names(dec.9.2016)[names(dec.9.2016) == "Moving.Time"] <- "Time"
@@ -42,6 +45,7 @@ names(mar.3.2017)[names(mar.3.2017) == "Moving.Time"] <- "Time"
 names(dec.7.2017)[names(dec.7.2017) == "Moving.Time"] <- "Time"
 names(dec.16.2017)[names(dec.16.2017) == "Moving.Time"] <- "Time"
 names(dec.21.2017)[names(dec.21.2017) == "Moving.Time"] <- "Time"
+names(dec.27.2017)[names(dec.27.2017) == "Moving.Time"] <- "Time"
 
 # Reindex rows to correct numbering
 rownames(dec.9.2016) <- 1:nrow(dec.9.2016)
@@ -52,6 +56,7 @@ rownames(mar.21.2017) <- 1:nrow(mar.21.2017)
 rownames(dec.7.2017) <- 1:nrow(dec.7.2017)
 rownames(dec.16.2017) <- 1:nrow(dec.16.2017)
 rownames(dec.21.2017) <- 1:nrow(dec.21.2017)
+rownames(dec.27.2017) <- 1:nrow(dec.27.2017)
 
 # Join data sets together
 all.workouts <- full_join(dec.9.2016, dec.23.2016, by = "Split") %>%
@@ -60,7 +65,8 @@ all.workouts <- full_join(dec.9.2016, dec.23.2016, by = "Split") %>%
   full_join(mar.21.2017, by = "Split") %>%
   full_join(dec.7.2017, by = "Split") %>%
   full_join(dec.16.2017, by = "Split") %>%
-  full_join(dec.21.2017, by = "Split")
+  full_join(dec.21.2017, by = "Split") %>%
+  full_join(dec.27.2017, by = "Split")
 
 # Rename column headers to date of workout
 names(all.workouts)[names(all.workouts) == "Time.x"] <- "Dec 9 2016"
@@ -71,6 +77,7 @@ names(all.workouts)[names(all.workouts) == "Time.x.x.x"] <- "Mar 21 2017"
 names(all.workouts)[names(all.workouts) == "Time.y.y.y"] <- "Dec 7 2017"
 names(all.workouts)[names(all.workouts) == "Time.x.x.x.x"] <- "Dec 16 2017"
 names(all.workouts)[names(all.workouts) == "Time.y.y.y.y"] <- "Dec 21 2017"
+names(all.workouts)[names(all.workouts) == "Time"] <- "Dec 27 2017"
 
 # Make new data table optimized for plotting
 all.workouts.plot <- all.workouts %>% gather(`Dec 9 2016`, 
@@ -81,6 +88,7 @@ all.workouts.plot <- all.workouts %>% gather(`Dec 9 2016`,
                                              `Dec 7 2017`, 
                                              `Dec 16 2017`, 
                                              `Dec 21 2017`,
+                                             `Dec 27 2017`,
                                              key = "Date", value = "Time")
 
 # Give workouts numbers
@@ -92,6 +100,7 @@ all.workouts.plot[5, 1] <- 5
 all.workouts.plot[6, 1] <- 6
 all.workouts.plot[7, 1] <- 7
 all.workouts.plot[8, 1] <- 8
+all.workouts.plot[9, 1] <- 9
 
 # New variable to allow grouping for plot
 all.workouts.plot[1, 4] <- 1
