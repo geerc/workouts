69acd6ac2838516342325e9c7e61f5c412a0b549
Christian Geer <geerc@allegheny.edu>
2018-02-03 11:16:09 -0500

    fixed 150 repeat problems, started work on other two workouts

--- Track/150 repeats/150 repeats script.R
+++ Track/150 repeats/150 repeats script.R
@@ -5,18 +5,22 @@ library(dplyr)
 # Import data sets for workouts
 feb.2.2017 <- read.csv("~/workouts/Track/150 repeats/feb 2 2017.csv")
 apr.25.2017 <- read.csv("~/workouts/Track/150 repeats/apr 25 2017.csv")
+feb.1.2018 <- read.csv("~/workouts/Track/150 repeats/feb 1 2018.csv")
 
 # Copy Time data to Moving.Time, so we can delete rest splits
 feb.2.2017 <- mutate(feb.2.2017, Moving.Time = Time)
-apr.25.2017 <- mutate(apr.25.2017, Moving.Time = Time)
+apr.25.2017 <- mutate(apr.25.2017, Moving.Time= Time)
+feb.1.2018 <- mutate(feb.1.2018, Moving.Time = Time)
 
 # Delete rest splits and remove unnecessary columns
 feb.2.2017 <- feb.2.2017[-c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21), ] %>% select(Split, Moving.Time)
-apr.25.2017 <- apr.25.2017[-c(2, 4, 6, 8, 10, 12, 14, 15), ] %>% select(Split, Moving.Time)
+apr.25.2017 <- apr.25.2017[-c(2, 4, 6, 8, 10, 12, 14, 15),  ] %>% select(Split, Moving.Time)
+feb.1.2018 <- feb.1.2018[-c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21), ] %>% select(Split, Moving.Time)
 
 # Rename Moving.Time to Time
 names(feb.2.2017)[names(feb.2.2017) == "Moving.Time"] <- "Time"
 names(apr.25.2017)[names(apr.25.2017) == "Moving.Time"] <- "Time"
+names(feb.1.2018)[names(feb.1.2018) == "Moving.Time"] <- "Time"
 
 # Rename splits to go count up from 1
 feb.2.2017[1, 1] <- 1
@@ -38,20 +42,37 @@ apr.25.2017[5, 1] <- 5
 apr.25.2017[6, 1] <- 6
 apr.25.2017[7, 1] <- 7
 
+feb.1.2018[1, 1] <- 1
+feb.1.2018[2, 1] <- 2
+feb.1.2018[3, 1] <- 3
+feb.1.2018[4, 1] <- 4
+feb.1.2018[5, 1] <- 5
+feb.1.2018[6, 1] <- 6
+feb.1.2018[7, 1] <- 7
+feb.1.2018[8, 1] <- 8
+feb.1.2018[9, 1] <- 9
+feb.1.2018[10, 1] <- 10
+
 
 # Reindex rows to correct numbering
 rownames(feb.2.2017) <- 1:nrow(feb.2.2017)
 rownames(apr.25.2017) <- 1:nrow(apr.25.2017)
+rownames(feb.1.2018) <- 1:nrow(feb.1.2018)
 
 # Join sets together
-all.workouts <- full_join(feb.2.2017, apr.25.2017, by = "Split")
+all.workouts <- full_join(feb.2.2017, apr.25.2017, by = "Split") %>%
+  full_join(feb.1.2018, by = "Split")
 
 # Rename column headers to date of workout
 names(all.workouts)[names(all.workouts) == "Time.x"] <- "Feb 2 2017"
 names(all.workouts)[names(all.workouts) == "Time.y"] <- "Apr 25 2017"
+names(all.workouts)[names(all.workouts) == "Time"] <- "Feb 1 2018"
 
 # Make new data table optimized for plotting
-all.workouts.plot <- all.workouts %>% gather(`Feb 2 2017`, `Apr 25 2017`, key = "Date", value = "Time") %>% 
+all.workouts.plot <- all.workouts %>% gather(`Feb 2 2017`,
+                                             `Apr 25 2017`,
+                                             `Feb 1 2018`,
+                                             key = "Date", value = "Time") %>% 
   arrange(Split)
 
 # Convert splits to doubles
@@ -61,6 +82,6 @@ all.workouts.plot$Split <- as.double(all.workouts.plot$Split)
 all.workouts.plot <- arrange(all.workouts.plot, Split)
 
 # Plot
-ggplot(data = all.workouts.plot, mapping = aes(x = Split, y = Time, group = Date, color = Date)) +
+ggplot(data = all.workouts.plot, mapping = aes(x = Split, y = Time, group = Date, color = Date, shape = Date)) +
   geom_path() +
   geom_point()
--- Track/300-200-200/300-200-200.R
+++ Track/300-200-200/300-200-200.R
@@ -9,10 +9,25 @@ jan.25.2018 <- read.csv("~/workouts/track/300-200-200/jan 25 2018.csv")
 jan.25.2018 <- mutate(jan.25.2018, Moving.Time = Time)
 
 # Delete rest splits and remove unnecessary columns
-jan.25.2018 <- jan.25.2018[-c(1:18), ] %>% select(Split, Moving.Time)
+jan.25.2018 <- jan.25.2018[-c(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 25), ] %>% select(Split, Moving.Time)
 
 # Rename Moving.Time to Time
 names(jan.25.2018)[names(jan.25.2018) == "Moving.Time"] <- "Time"
+
+# Renaming splits to go in order
+jan.25.2018[1, 1] <- 1
+jan.25.2018[2, 1] <- 2
+jan.25.2018[3, 1] <- 3
+jan.25.2018[4, 1] <- 4
+jan.25.2018[5, 1] <- 5
+jan.25.2018[6, 1] <- 6
+jan.25.2018[7, 1] <- 7
+jan.25.2018[8, 1] <- 8
+jan.25.2018[9, 1] <- 9
+jan.25.2018[10, 1] <- 10
+jan.25.2018[11, 1] <- 11
+jan.25.2018[12, 1] <- 12
+
 
 # Reindex rows to correct numbering
 rownames(jan.25.2018) <- 1:nrow(jan.25.2018)
@@ -33,6 +48,12 @@ all.workouts.plot <- all.workouts %>% gather(`Jan 25 2018`,
 all.workouts.plot$Split <- as.double(all.workouts.plot$Split)
 all.workouts.plot <- arrange(all.workouts.plot, Split)
 
+jan.25.2018$Split <- as.double(jan.25.2018$Split)
+jan.25.2018 <- arrange(jan.25.2018, Split)
+
 ggplot(data = all.workouts.plot, mapping = aes(x = Split, y = Time, group = V4)) +
   geom_path() +
+  geom_point()
+
+ggplot(data = jan.25.2018, mapping = aes(x = Split, y = Time)) + 
   geom_point()
