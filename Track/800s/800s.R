e9ea9608e00a10f657666d0b3c147670034e6d7b
Christian Geer <geerc@allegheny.edu>
2018-02-18 19:45:54 -0500

    added mount 800 and made some small changes

--- Track/800s/800s.R
+++ Track/800s/800s.R
@@ -10,6 +10,7 @@ kenyon.2017 <- read.csv("~/workouts/Track/800s/kenyon 2017.csv")
 gator.2017 <- read.csv("~/workouts/Track/800s/gator 2017.csv")
 wooster.2018 <- read.csv("~/workouts/Track/800s/wooster 2018.csv")
 mount.union.2018 <- read.csv("~/workouts/Track/800s/mount union 2018.csv")
+mount.union.2018.2 <- read.csv("~/workouts/Track/800s/mount union 2018 2.csv")
 
 # Copy Time data to Moving.Time, so we can delete rest splits
 spire.2017 <- mutate(spire.2017, Moving.Time = Time)
@@ -19,6 +20,7 @@ kenyon.2017 <- mutate(kenyon.2017, Moving.Time = Time)
 gator.2017 <- mutate(gator.2017, Moving.Time = Time)
 wooster.2018 <- mutate(wooster.2018, Moving.Time = Time)
 mount.union.2018 <- mutate(mount.union.2018, Moving.Time = Time)
+mount.union.2018.2 <- mutate(mount.union.2018.2, Moving.Time = Time)
 
 # Delete rest splits and remove unnecessary columns
 spire.2017 <- spire.2017[-c(2), ] %>% select(Split, Moving.Time)
@@ -28,6 +30,7 @@ kenyon.2017 <- kenyon.2017[-c(2), ] %>% select(Split, Moving.Time)
 gator.2017 <- gator.2017[-c(2), ] %>% select(Split, Moving.Time)
 wooster.2018 <- wooster.2018[-c(2), ] %>% select(Split, Moving.Time)
 mount.union.2018 <- mount.union.2018[-c(2), ] %>% select(Split, Moving.Time)
+mount.union.2018.2 <- mount.union.2018.2[-c(2), ] %>% select(Split, Moving.Time)
 
 # Rename Moving.Time to Time
 names(spire.2017)[names(spire.2017) == "Moving.Time"] <- "Time"
@@ -37,6 +40,7 @@ names(kenyon.2017)[names(kenyon.2017) == "Moving.Time"] <- "Time"
 names(gator.2017)[names(gator.2017) == "Moving.Time"] <- "Time"
 names(wooster.2018)[names(wooster.2018) == "Moving.Time"]<- "Time"
 names(mount.union.2018)[names(mount.union.2018) == "Moving.Time"] <- "Time"
+names(mount.union.2018.2)[names(mount.union.2018.2) == "Moving.Time"] <- "Time"
 
 # Join data sets together
 all.workouts <- full_join(spire.2017, mount.union.2017, by = "Split") %>%
@@ -44,7 +48,8 @@ all.workouts <- full_join(spire.2017, mount.union.2017, by = "Split") %>%
   full_join(kenyon.2017, by = "Split") %>%
   full_join(gator.2017, by = "Split") %>%
   full_join(wooster.2018, by = "Split") %>%
-  full_join(mount.union.2018, by = "Split")
+  full_join(mount.union.2018, by = "Split") %>%
+  full_join(mount.union.2018.2, by = "Split")
 
 # Rename column headers to date of workout
 names(all.workouts)[names(all.workouts) == "Time.x"] <- "Spire 2017"
@@ -53,7 +58,8 @@ names(all.workouts)[names(all.workouts) == "Time.x.x"]   <- "Tiffin 2017"
 names(all.workouts)[names(all.workouts) == "Time.y.y"] <- "Kenyon 2017"
 names(all.workouts)[names(all.workouts) == "Time.x.x.x"] <- "Gator 2017"
 names(all.workouts)[names(all.workouts) == "Time.y.y.y"] <- "Wooster 2018"
-names(all.workouts)[names(all.workouts) == "Time"] <- "Mount Union 2018"
+names(all.workouts)[names(all.workouts) == "Time.x.x.x.x"] <- "Mount Union 2018"
+names(all.workouts)[names(all.workouts) == "Time.y.y.y.y"] <- "Mount Union 2018 2"
 
 # Make new data table optimized for plotting
 all.workouts.plot <- all.workouts %>% gather(`Spire 2017`,
@@ -63,6 +69,7 @@ all.workouts.plot <- all.workouts %>% gather(`Spire 2017`,
                                              `Gator 2017`,
                                              `Wooster 2018`,
                                              `Mount Union 2018`,
+                                             `Mount Union 2018 2`,
                                              key = "Date", value = "Time") %>% 
   arrange(Split)
 
@@ -77,6 +84,7 @@ all.workouts.plot[4, 1] <- 4
 all.workouts.plot[5, 1] <- 5
 all.workouts.plot[6, 1] <- 6
 all.workouts.plot[7, 1] <- 7
+all.workouts.plot[8, 1] <- 8
 
 # New variable to allow grouping for plot
 all.workouts.plot[1, 4] <- 1
